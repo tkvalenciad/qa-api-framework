90b53a0797d08f2fbf62c26923aecd84d9336317
tkvalenciad <tkvalenciad@gmail.com>
2026-06-10 15:03:52 -0500

    Add smoke and regression test markers

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,8 @@
+import pytest
 from api.user_api import UserAPI
 
 
+@pytest.mark.smoke
 def test_get_users():
 
     api = UserAPI()
@@ -8,3 +10,16 @@ def test_get_users():
     response = api.get_users()
 
     assert response.status_code == 200
+
+@pytest.mark.regression
+def test_users_response_contains_data():
+
+    api = UserAPI()
+
+    response = api.get_users()
+
+    body = response.json()
+
+    assert "id" in body[0]
+    assert "name" in body[0]
+    assert "email" in body[0]
